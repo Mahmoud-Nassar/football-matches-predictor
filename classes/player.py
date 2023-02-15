dfb3f44b6a13a62cf5d39af36e8ca795ccf5f366
Mahmoud Nassar <Mahmoud-Nassar@users.noreply.github.com>
2023-02-15 20:07:04 +0200

    Done with the DTClassifer (sort of, still need to make the precision better). now getting started with KNN, this version cotaiens, KNN first version .

--- classes/player.py
+++ classes/player.py
@@ -1,8 +1,36 @@
+import csv
+import numpy as np
 
 
+def extractPlayers():
+    games = np.genfromtxt('dataSet\season_17_18\games\\games.csv'
+                          , delimiter=',', names=True, dtype=None, encoding=None)
+    csvFile = [["player", "team"]]
+    for game in games:
+        for i in range(1, 3):
+            team = game[i]
+            for j in range(4, 15):
+                csvFile.append([getPlayerName(game[j + ((i - 1) * 14)]), team])
+    csvFile = remove_duplicates(csvFile)
+    with open('dataSet\season_17_18\players\\' + 'players.csv',
+              'w', newline='') as playersFiles:
+        writer = csv.writer(playersFiles, delimiter=',')
+        writer.writerows(csvFile)
 
-# class Player:
-#     def __init__(self,id,name,rank):
-#         self.id=id
-#         self.name= name
-#         self.rank=rank
+
+def remove_duplicates(arr):
+    result = []
+    for sub_arr in arr:
+        if sub_arr not in result:
+            result.append(sub_arr)
+    return result
+
+
+def getPlayerName(playerName):
+    i = 0
+    result = ""
+    length = len(playerName)
+    while playerName[i] != ')' and playerName[i] != ' ' and i < length - 1:
+        result += playerName[i]
+        i += 1
+    return result
--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 from classes.League import League
-from classifcation.decisionTree import DTClassifier as dt
+from classification.decisionTree import DTClassifier as dt
+from classification.KNearestNeighbors import KNNClassifier as knn
 from scrapping.PlayerRank import get_fifa_rating
+from classes.Player import extractPlayers
 
 csvTeamsPath = "dataSet\season_17_18\\teams\\teams.csv"
 csvGamesPath = "dataSet\season_17_18\games\\games.csv"
 csvWritePath = "dataSet\\"
 
 if __name__ == '__main__':
-    # # process data
-    # league = League(csvTeamsPath, csvGamesPath, csvWritePath)
-    #
-    # print("Decision Tree: ")
-    # print("    basic classification precision: {:.2f}%"
-    #       .format(dt.decisionTreeBasicClassification()))
-    #
-    # er1 = dt.experimentOnTestSize([0.1, 0.2, 0.3, 0.4])
-    # print("    test size experiment: best test size is {}% of the data "
-    #       ",and it`s resulted precision is: {:.2f}%"
-    #       .format(int(er1[0] * 100), er1[1]))
-    #
-    # er2 = dt.experimentOnMaxDepth([2, 3, 4, 5])
-    # print("    maximum depth experiment: the best max depth is {}"
-    #       ",and it`s resulted precision is: {:.2f}%"
-    #       .format(int(er2[0]), er2[1]))
-    #
-    # print(" best precision using all the experiments result: {:.2f}%"
-    #       .format(dt.getBestPrecision(er2[0], er1[0])))
-    print(get_fifa_rating("Bergara M."))
+    ##################################################################
+    ####################### DECISION-TREE  ###########################
+    # process data
+    league = League(csvTeamsPath, csvGamesPath, csvWritePath)
+
+    print("Decision Tree: ")
+    print("        -basic classification precision: {:.2f}%"
+          .format(dt.decisionTreeBasicClassification()))
+
+    er1 = dt.experimentOnTestSize([0.1, 0.2, 0.3, 0.4])
+    print("        -test size experiment: best test size is {}% of the data "
+          ",and it`s resulted precision is: {:.2f}%"
+          .format(int(er1[0] * 100), er1[1]))
+
+    er2 = dt.experimentOnMaxDepth([2, 3, 4, 5])
+    print("        -maximum depth experiment: the best max depth is {}"
+          ",and it`s resulted precision is: {:.2f}%"
+          .format(int(er2[0]), er2[1]))
+
+    print("    -best precision considering all experiments results: {:.2f}%"
+          .format(dt.getBestPrecision(er2[0], er1[0])))
+    ##################################################################
+    ####################### KNN  #####################################
+    # process data
+    league = League(csvTeamsPath, csvGamesPath, csvWritePath)
+    print("KNN: ")
+    print("        -basic classification precision: {:.2f}%"
+          .format(knn.knnBasicClassification()))
+
+    er3 = knn.experimentOnTestSize([0.1, 0.2, 0.3, 0.4])
+    print("        -test size experiment: best test size is {}% of the data "
+          ",and it`s resulted precision is: {:.2f}%"
+          .format(int(er3[0] * 100), er3[1]))
+
+    er4 = knn.experimentOnNNeighbors([3, 4, 5, 6])
+    print("        -number of nearst neighbors experiment: the best "
+          "number of neighbors is {}"
+          ",and it`s \n         resulted precision is: {:.2f}%"
+          .format(int(er4[0]), er4[1]))
+
+    print("    -best precision considering all experiments results: {:.2f}%"
+          .format(dt.getBestPrecision(er4[0], er3[0])))
